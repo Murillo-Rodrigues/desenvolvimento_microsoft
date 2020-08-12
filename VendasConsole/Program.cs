ef245e32b9b20480aca252dd7e74bf3f1beef68a
Murillo Rodrigues <murillorodriguesdsouza@gmail.com>
2020-08-12 17:56:24 -0300

    Adicionar menu na aplicação

--- VendasConsole/Program.cs
+++ VendasConsole/Program.cs
@@ -6,7 +6,62 @@ namespace VendasConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int opcao;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("" +
+                    "\n ---- APLICAÇÃO DE VENDAS ----" +
+                    "\n1 - Cadastrar Cliente" +
+                    "\n2 - Listar Clientes" +
+                    "\n0 - Sair");
+
+                Console.WriteLine("\nDigite a opção desejada: ");
+                opcao = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+
+                switch (opcao)
+                {
+                    case 1:
+                        Console.WriteLine(" ---- Cadastrar Cliente ---- \n\n");
+                        break;
+                    case 2:
+                        Console.WriteLine(" ---- Listar Clientes ---- \n\n");
+                        break;
+                    case 3:
+
+                        break;
+                    case 4:
+
+                        break;
+                    case 5:
+
+                        break;
+                    case 6:
+
+                        break;
+                    case 7:
+
+                        break;
+                    case 8:
+
+                        break;
+                    case 9:
+
+                        break;
+                    case 0:
+                        Console.WriteLine(" ---- Saindo ---- \n\n");
+                        break;
+                    default:
+                        Console.WriteLine(" ---- OPÇÃO INVÁLIDA ---- \n\n");
+                        break;
+                }
+                Console.WriteLine("Pressione uma tecla para continuar...");
+                Console.ReadKey();
+            } while (opcao != 0);
+
+
         }
     }
 }
