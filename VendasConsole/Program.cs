6cdcb790937d3ca1578011f714266105b8b8d01a
Murillo Rodrigues <murillorodriguesdsouza@gmail.com>
2020-08-17 17:14:13 -0300

    Cadastrar/Listar Vendedores e Produtos

--- VendasConsole/Program.cs
+++ VendasConsole/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace VendasConsole
 {
@@ -10,7 +9,11 @@ namespace VendasConsole
         {
             int opcao;
             Cliente c = new Cliente();
+            Vendedor v = new Vendedor();
+            Produto p = new Produto();
             List<Cliente> clientes = new List<Cliente>();
+            List<Vendedor> vendedores = new List<Vendedor>();
+            List<Produto> produtos = new List<Produto>();
 
 
             do
@@ -20,6 +23,10 @@ namespace VendasConsole
                     "\n ---- APLICAÇÃO DE VENDAS ----" +
                     "\n1 - Cadastrar Cliente" +
                     "\n2 - Listar Clientes" +
+                    "\n3 - Cadastrar Vendedor" +
+                    "\n4 - Listar Vendedores" +
+                    "\n5 - Cadastrar Produto" +
+                    "\n6 - Listar Produtos" +
                     "\n0 - Sair");
 
                 Console.WriteLine("\nDigite a opção desejada: ");
@@ -75,16 +82,97 @@ namespace VendasConsole
                         }
                         break;
                     case 3:
+                        v = new Vendedor();
+                        Console.WriteLine(" ---- Cadastrar Vendedor ---- \n\n");
+
+                        Console.WriteLine("Insira o nome do vendedor: ");
+                        v.Nome = Console.ReadLine();
+                        Console.WriteLine("\nInsira o CPF do vendedor: ");
+                        v.Cpf = Console.ReadLine();
+
+                        if (vendedores.Count == 0)
+                        {
+                            vendedores.Add(v);
+                            Console.WriteLine("\n\n ---- Vendedor cadastrado com sucesso! ----\n\n");
+
+                        }
+                        else
+                        {
+                            bool encontrado = false;
+                            foreach (Vendedor vendedorCadastrado in vendedores)
+                            {
+                                if (vendedorCadastrado.Cpf == v.Cpf)
+                                {
+                                    encontrado = true;
+                                }
+
+                            }
+                            if (!encontrado)
+                            {
+                                vendedores.Add(v);
+                                Console.WriteLine("\n\n ---- Vendedor cadastrado com sucesso! ----\n\n");
+                            }
+                            else
+                            {
+                                Console.WriteLine("\n\nNão foi possível cadastrar!\n\n");
+                            }
+
+                        }
 
                         break;
                     case 4:
-
+                        Console.WriteLine(" ---- Listar Vendedores ---- \n\n");
+                        foreach (Vendedor vendedorCadastrado in vendedores)
+                        {
+                            Console.WriteLine(vendedorCadastrado);
+                        }
                         break;
                     case 5:
+                        p = new Produto();
+                        Console.WriteLine(" ---- Cadastrar Produto ---- \n\n");
 
+                        Console.WriteLine("Insira o nome do produto: ");
+                        p.Nome = Console.ReadLine();
+                        Console.WriteLine("\nInsira o preço do produto: ");
+                        p.Preco = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("\nInsira a quantidade do produto: ");
+                        p.Quantidade = Convert.ToInt32(Console.ReadLine());
+
+                        if (produtos.Count == 0)
+                        {
+                            produtos.Add(p);
+                            Console.WriteLine("\n\n ---- Produto cadastrado com sucesso! ----\n\n");
+
+                        }
+                        else
+                        {
+                            bool encontrado = false;
+                            foreach (Produto produtoCadastrado in produtos)
+                            {
+                                if (produtoCadastrado.Nome == p.Nome)
+                                {
+                                    encontrado = true;
+                                }
+
+                            }
+                            if (!encontrado)
+                            {
+                                produtos.Add(p);
+                                Console.WriteLine("\n\n ---- Produto cadastrado com sucesso! ----\n\n");
+                            }
+                            else
+                            {
+                                Console.WriteLine("\n\nNão foi possível cadastrar!\n\n");
+                            }
+
+                        }
                         break;
                     case 6:
-
+                        Console.WriteLine(" ---- Listar Produtos ---- \n\n");
+                        foreach (Produto produtoCadastrado in produtos)
+                        {
+                            Console.WriteLine(produtoCadastrado);
+                        }
                         break;
                     case 7:
 
