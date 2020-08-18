fb8b1192948120b2a60f1a49caa8fd6220add860
Murillo Rodrigues <murillorodriguesdsouza@gmail.com>
2020-08-17 21:36:15 -0300

    Validação de CPF

--- VendasConsole/Program.cs
+++ VendasConsole/Program.cs
@@ -44,35 +44,41 @@ namespace VendasConsole
                         Console.WriteLine("\nInsira o CPF do cliente: ");
                         c.Cpf = Console.ReadLine();
 
-                        if (clientes.Count == 0)
+                        if (ValidarCpf(c.Cpf))
                         {
-                            clientes.Add(c);
-                            Console.WriteLine("\n\n ---- Cliente cadastrado com sucesso! ----\n\n");
-
-                        }
-                        else
-                        {
-                            bool encontrado = false;
-                            foreach (Cliente clienteCadastrado in clientes)
-                            {
-                                if (clienteCadastrado.Cpf == c.Cpf)
-                                {
-                                    encontrado = true;
-                                }
-
-                            }
-                            if (!encontrado)
+                            if (clientes.Count == 0)
                             {
                                 clientes.Add(c);
                                 Console.WriteLine("\n\n ---- Cliente cadastrado com sucesso! ----\n\n");
+
                             }
                             else
                             {
-                                Console.WriteLine("\n\nNão foi possível cadastrar!\n\n");
+                                bool encontrado = false;
+                                foreach (Cliente clienteCadastrado in clientes)
+                                {
+                                    if (clienteCadastrado.Cpf == c.Cpf)
+                                    {
+                                        encontrado = true;
+                                    }
+
+                                }
+                                if (!encontrado)
+                                {
+                                    clientes.Add(c);
+                                    Console.WriteLine("\n\n ---- Cliente cadastrado com sucesso! ----\n\n");
+                                }
+                                else
+                                {
+                                    Console.WriteLine("\n\nNão foi possível cadastrar!\n\n");
+                                }
+
                             }
-
                         }
-
+                        else
+                        {
+                            Console.WriteLine("\n\nNão foi possível cadastrar!\n\n");
+                        }
                         break;
                     case 2:
                         Console.WriteLine(" ---- Listar Clientes ---- \n\n");
@@ -195,6 +201,73 @@ namespace VendasConsole
             } while (opcao != 0);
 
 
+        }
+
+        private static bool ValidarCpf(string cpf)
+        {
+            int peso = 10, soma = 0, resto, digito1, digito2;
+            cpf = cpf.Replace(".", "").Replace("-", "");
+            if (cpf.Length != 11)
+            {
+                return false;
+            }
+            switch (cpf)
+            {
+                case "11111111111": return false;
+                case "22222222222": return false;
+                case "33333333333": return false;
+                case "44444444444": return false;
+                case "55555555555": return false;
+                case "66666666666": return false;
+                case "77777777777": return false;
+                case "88888888888": return false;
+                case "99999999999": return false;
+                case "00000000000": return false;
+            }
+
+            //Digito 1
+            for (int i = 0; i < 9; i++)
+            {
+                soma += Convert.ToInt32(cpf[i].ToString()) * peso;
+                peso--;
+            }
+            resto = soma % 11;
+            if (resto < 2)
+            {
+                digito1 = 0;
+            }
+            else
+            {
+                digito1 = 11 - resto;
+            }
+            if (Convert.ToInt32(cpf[9].ToString()) != digito1)
+            {
+                return false;
+            }
+
+            //Digito 2
+            peso = 11;
+            soma = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                soma += Convert.ToInt32(cpf[i].ToString()) * peso;
+                peso--;
+            }
+            resto = soma % 11;
+            if (resto < 2)
+            {
+                digito2 = 0;
+            }
+            else
+            {
+                digito2 = 11 - resto;
+            }
+            if (Convert.ToInt32(cpf[10].ToString()) != digito2)
+            {
+                return false;
+            }
+
+            return true;
         }
     }
 }
